cfd50e8ddf32932c2478aad6065f88ff3db33bc5
Ali <30610753+aamining@users.noreply.github.com>
2019-07-21 14:29:13 +1000

    create QA component and rendering, create QA data folder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import QA from './components/QA';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import data from './data/data.json';
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+      this.state = {
+        qas: []      
+      }  
+  }
+
+  componentDidMount () {
+    this.setState({qas: data});
+  }
+
+  render() {
+    return (
+      
+      <div className="App">
+        <Router>
+          <div>
+              {
+                <Route path = "/" render={() => this.state.qas.map((qa) => (<QA key={qa.id} qa={qa}/>))}/> 
+              }
+
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
+
---
+++ my-app/src/components/QA.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './QA.css';
+
+
+function QA (props){
+    
+    return(
+        <div>
+        <h1>Hello from QA component </h1>
+
+            
+        </div>
+
+    )
+}
+
+export default QA;
